16a3baa0a1dcb55e780541bd0a98376e6ddee6c5
tanjoshua <jtanjoshua@gmail.com>
2021-01-19 19:50:03 -0500

    edituser controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -6,6 +6,10 @@ exports.getUser = (req, res, next) => {
   const userId = req.params.userId;
 };
 
+exports.editUser = (req, res, next) => {
+  const userId = req.params.userId;
+};
+
 exports.getUsers = (req, res, next) => {
   const userIds = req.body.userIds; // array of userIds
 };
