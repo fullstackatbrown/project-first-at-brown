5f627635d7d26b9ffd29fa5bf2300274c896b8a6
Taishi <taishi.nishizawa2001@gmail.com>
2021-02-15 11:28:17 -0500

    getUser and editUser

--- server/controllers/accountController.js
+++ server/controllers/accountController.js
@@ -41,7 +41,6 @@ exports.signup = async (req, res, next) => {
       pronouns,
       email,
     });
-
     // can use
     const token = jwt.sign({ email, userId }, process.env.JWT_KEY);
 
@@ -51,14 +50,39 @@ exports.signup = async (req, res, next) => {
   }
 };
 
-exports.getAccount = (req, res, next) => {
+exports.getAccount = async (req, res, next) => {
   const accountId = req.params.accountId;
+  try {
+    const userId = await account.read(accountId);
+  } catch (err) {
+    return next(err);
+  }
 };
 
-exports.editAccount = (req, res, next) => {
+exports.editAccount = async (req, res, next) => {
   const accountId = req.params.accountId;
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const year = req.body.year;
+  const picture = req.body.picture;
+  const concentration = req.body.concentration;
+  const pronouns = req.body.pronouns;
+
+  try {
+    const userId = await account.update({
+      first_name: firstName,
+      last_name: lastName,
+      year,
+      picture,
+      concentration,
+      pronouns,
+      accountId,
+    });
+  } catch (err) {
+    return next(err);
+  }
 };
 
-exports.getAccounts = (req, res, next) => {
+exports.getAccounts = async (req, res, next) => {
   const accountIds = req.body.accountIds; // array of accountIds
 };
