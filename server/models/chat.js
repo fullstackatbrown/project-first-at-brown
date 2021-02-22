42421d82c6307969d92d1814b12299a5b74fc285
Jason Crowley <crowley.json@gmail.com>
2021-02-22 12:36:53 -0500

    Move message.read() -> chat.read() and sort messages by most recent first

--- server/models/chat.js
+++ server/models/chat.js
@@ -1,12 +1,21 @@
 const db = require('../config/db');
 
+exports.read = ({ account_id1, account_id2 }) => db.any(
+  `SELECT *
+      FROM message
+      WHERE (sender_id = $1 AND recipient_id = $2)
+      OR    (sender_id = $2 AND recipient_id = $1)
+      ORDER BY created_at DESC`,
+  [account_id1, account_id2]
+);
+
 exports.list = (account_id) => db.any(
   `SELECT *
       FROM message
       WHERE sender_id = $1 OR recipient_id = $1
-      ORDER BY created_at`,
+      ORDER BY created_at DESC`,
   [account_id]
-).then(messages => {
+).then((messages) => {
   const chats = new Map();
   for (const message of messages) {
     const { sender_id, recipient_id } = message;
--- server/models/message.js
+++ server/models/message.js
@@ -6,10 +6,3 @@ exports.create = ({ body, sender_id, recipient_id }) => db.one(
       RETURNING message_id`,
   [body, sender_id, recipient_id]
 );
-
-exports.read = ({ sender_id, recipient_id }) => db.any(
-  `SELECT *
-      FROM message
-      WHERE sender_id = $1 AND recipient_id = $2`,
-  [sender_id, recipient_id]
-);
