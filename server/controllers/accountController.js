802a9a6f019b09da270c22e8514dd3e7ef04a4ab
tanjoshua <jtanjoshua@gmail.com>
2021-02-21 20:26:45 -0500

    async handler

--- server/controllers/accountController.js
+++ server/controllers/accountController.js
@@ -1,25 +1,22 @@
 const jwt = require("jsonwebtoken");
 // const { validationResult } = require("express-validator");
+const asyncHandler = require("express-async-handler");
 
 const account = require("../models/account");
 
-exports.login = async (req, res, next) => {
+exports.login = asyncHandler(async (req, res, next) => {
   const token = req.body.token;
 
-  try {
-    const result = await account.login(token);
-    const accountId = result.account_id;
+  const result = await account.login(token);
+  const accountId = result.account_id;
 
-    // create jwt
-    const jwtoken = jwt.sign({ accountId }, process.env.JWT_KEY);
+  // create jwt
+  const jwtoken = jwt.sign({ accountId }, process.env.JWT_KEY);
 
-    res.json({ token: jwtoken, accountId });
-  } catch (error) {
-    return next(error);
-  }
-};
+  res.json({ token: jwtoken, accountId });
+});
 
-exports.signup = async (req, res, next) => {
+exports.signup = asyncHandler(async (req, res, next) => {
   const firstName = req.body.firstName;
   const lastName = req.body.lastName;
   const year = req.body.year;
@@ -29,39 +26,30 @@ exports.signup = async (req, res, next) => {
   const email = req.body.email;
   const token = req.body.token;
 
-  try {
-    const result = await account.create({
-      first_name: firstName,
-      last_name: lastName,
-      year,
-      concentration,
-      pronouns,
-      email,
-      token,
-      picture,
-    });
+  const result = await account.create({
+    first_name: firstName,
+    last_name: lastName,
+    year,
+    concentration,
+    pronouns,
+    email,
+    token,
+    picture,
+  });
 
-    const accountId = result.account_id;
-    // can use
-    const jwtoken = jwt.sign({ email, accountId }, process.env.JWT_KEY);
+  const accountId = result.account_id;
+  // can use
+  const jwtoken = jwt.sign({ email, accountId }, process.env.JWT_KEY);
 
-    res.status(201).json({ token: jwtoken, accountId });
-  } catch (err) {
-    console.log(err);
-    return next(err);
-  }
-};
+  res.status(201).json({ token: jwtoken, accountId });
+});
 
-exports.getAccount = async (req, res, next) => {
+exports.getAccount = asyncHandler(async (req, res, next) => {
   const accountId = req.params.accountId;
-  try {
-    const userId = await account.read(accountId);
-  } catch (err) {
-    return next(err);
-  }
-};
+  const userId = await account.read(accountId);
+});
 
-exports.editAccount = async (req, res, next) => {
+exports.editAccount = asyncHandler(async (req, res, next) => {
   const accountId = req.params.accountId;
   const firstName = req.body.firstName;
   const lastName = req.body.lastName;
@@ -70,21 +58,17 @@ exports.editAccount = async (req, res, next) => {
   const concentration = req.body.concentration;
   const pronouns = req.body.pronouns;
 
-  try {
-    const userId = await account.update({
-      first_name: firstName,
-      last_name: lastName,
-      year,
-      picture,
-      concentration,
-      pronouns,
-      accountId,
-    });
-  } catch (err) {
-    return next(err);
-  }
-};
+  const userId = await account.update({
+    first_name: firstName,
+    last_name: lastName,
+    year,
+    picture,
+    concentration,
+    pronouns,
+    accountId,
+  });
+});
 
-exports.getAccounts = async (req, res, next) => {
+exports.getAccounts = asyncHandler(async (req, res, next) => {
   const accountIds = req.body.accountIds; // array of accountIds
-};
+});
