e67af0ef502e7239e7504712f265054be3e113cc
Johnny Roy <jkairoy@gmail.com>
2021-04-05 12:11:02 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -52,7 +52,7 @@ app.use("/", (req, res, next) => {
 app.use((err, req, res, next) => {
   const status = err.statusCode || 500;
   const message = err.message;
-
+  console.error(err)
   res.status(status).json({ message });
 });
 
