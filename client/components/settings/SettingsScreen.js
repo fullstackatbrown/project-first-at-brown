618f45a47cc5ccb60ecfce7f266c1ff3c4a9c98e
tanjoshua <jtanjoshua@gmail.com>
2021-05-22 20:05:56 +0800

    fetching account details from backend

--- client/components/settings/SettingsScreen.js
+++ client/components/settings/SettingsScreen.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { useSelector } from "react-redux";
+import API from "../../api";
 
 const SettingsScreen = () => {
+  const { accountId, token } = useSelector((state) => state.auth);
+  const [accountDetails, setAccountDetails] = useState({});
+
+  // load user details
+  useEffect(() => {
+    const fetchAccountDetails = async () => {
+      const response = await API.get(`account/${accountId}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      setAccountDetails(response.data);
+    };
+
+    fetchAccountDetails();
+  }, []);
+
   return (
     <View>
-      <Text>Settings</Text>
+      <Text>{accountDetails.first_name}</Text>
     </View>
   );
 };
