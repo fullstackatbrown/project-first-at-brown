817d90bbb76f45bc34deef215a015d817de2ead5
jkairoy <jkairoy@gmail.com>
2021-11-13 13:32:11 -0500

    port to 80

--- server/server.js
+++ server/server.js
@@ -59,7 +59,7 @@ app.use((err, req, res) => {
 });
 
 // start server
-const server = app.listen(3000);
+const server = app.listen(80);
 console.log("Server listening")
 const io = require('socket.io')(server);
 app.set('socketio', io);
